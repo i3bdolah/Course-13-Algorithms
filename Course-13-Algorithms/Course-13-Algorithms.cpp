cf0653e1a6205c186fa467299324a1db5df4d445
i3bdolah <xbdullahx@gmail.com>
2024-06-17 23:32:07 +0300

    Extensions are implemented, and MyQueue Project is done.

--- Course-13-Algorithms/Course-13-Algorithms.cpp
+++ Course-13-Algorithms/Course-13-Algorithms.cpp
@@ -5,27 +5,70 @@ using namespace std;
 
 int main()
 {
-    clsMyQueue <int> MyQueue;
-    
-    MyQueue.Push(10);
-    MyQueue.Push(20);
-    MyQueue.Push(30);
-    MyQueue.Push(40);
-    MyQueue.Push(50);
+	clsMyQueue <int> MyQueue;
 
-    MyQueue.Print();
-    cout << "Queue Size  : " << MyQueue.Size() << endl;
-    cout << "Queue Front : " << MyQueue.Front() << endl;
-    cout << "Queue Back  : " << MyQueue.Back() << endl;
+	MyQueue.Push(10);
+	MyQueue.Push(20);
+	MyQueue.Push(30);
+	MyQueue.Push(40);
+	MyQueue.Push(50);
 
-    MyQueue.Pop();
+	MyQueue.Print();
+	cout << "Queue Size  : " << MyQueue.Size() << endl;
+	cout << "Queue Front : " << MyQueue.Front() << endl;
+	cout << "Queue Back  : " << MyQueue.Back() << endl;
 
-    MyQueue.Print();
+	MyQueue.Pop();
 
-    cout << "Queue Size  : " << MyQueue.Size() << endl;
-    cout << "Queue Front : " << MyQueue.Front() << endl;
-    cout << "Queue Back  : " << MyQueue.Back() << endl;
+	MyQueue.Print();
 
-    cout << "\n\n";
+	cout << "Queue Size  : " << MyQueue.Size() << endl;
+	cout << "Queue Front : " << MyQueue.Front() << endl;
+	cout << "Queue Back  : " << MyQueue.Back() << endl;
+
+
+	// Extension #1
+	cout << "GetItem(2) : " << endl;
+	cout << MyQueue.GetItem(2) << endl;
+
+	// Extension #2
+	MyQueue.Reverse();
+	cout << "After Reverse : ";
+	MyQueue.Print();
+	cout << "\n";
+
+	// Extension #3
+	MyQueue.UpdateItem(2, 600);
+	cout << "After UpdateItem(2,600) : ";
+	MyQueue.Print();
+	cout << "\n";
+
+	// Extension #4
+	MyQueue.InsertAfter(2, 800);
+	cout << "After InsertAfter(2, 800) : ";
+	MyQueue.Print();
+	cout << "\n";
+
+	// Extension #5
+	MyQueue.InsertAtFront(1000);
+	cout << "After InsertAtFront(1000) : ";
+	MyQueue.Print();
+	cout << "\n";
+
+	// Extension #6
+	MyQueue.InsertAtBack(2000);
+	cout << "After InsertAtBack(2000) : ";
+	MyQueue.Print();
+	cout << "\n";
+
+	// Extension #7
+	MyQueue.Clear();
+	cout << "After Clear() : ";
+	MyQueue.Print();
+	cout << "\n";
+	cout << "Is Empty (True or False) : " << MyQueue.IsEmpty();
+
+
+	cout << "\n\n";
 
 }
--- Course-13-Algorithms/clsMyQueue.h
+++ Course-13-Algorithms/clsMyQueue.h
@@ -8,38 +8,74 @@ class clsMyQueue
 {
 protected:
 
-	clsDblLinkedList <EnteredType> MyList;
+	clsDblLinkedList <EnteredType> _MyList;
 
 public:
 
 	void Print() {
-		MyList.PrintList();
+		_MyList.PrintList();
 	}
 
 	// -----------------------------------
 
 	void Push(EnteredType value) {
-		MyList.InsertAtEnd(value);
+		_MyList.InsertAtEnd(value);
 	}
 
 	void Pop() {
-		MyList.DeleteFirstNode();
+		_MyList.DeleteFirstNode();
 	}
 
 	int Size() {
-		return MyList.Size();
+		return _MyList.Size();
 	}
 
 	bool IsEmpty() {
-		return MyList.IsEmpty();
+		return _MyList.IsEmpty();
 	}
 
 	EnteredType Front() {
-		return MyList.GetItem(0);
+		return _MyList.GetItem(0);
 	}
 
 	EnteredType Back() {
-		return MyList.GetItem(Size() - 1);
+		return _MyList.GetItem(Size() - 1);
 	}
+
+	// Extension #1
+	EnteredType GetItem(int index) {
+		return _MyList.GetItem(index);
+	}
+
+	// Extension #2
+	void Reverse() {
+		_MyList.Reverse();
+	}
+
+	// Extension #3
+	void UpdateItem(int index, EnteredType value) {
+		_MyList.UpdateItem(index, value);
+	}
+
+	// Extension #4
+	void InsertAfter(int index, EnteredType value) {
+		_MyList.InsertAfter(index, value);
+	}
+
+	// Extension #5
+	void InsertAtFront(EnteredType value) {
+		_MyList.InsertAtBeginning(value);
+	}
+
+	// Extension #6
+	void InsertAtBack(EnteredType value) {
+		_MyList.InsertAtEnd(value);
+	}
+
+	// Extension #7
+	void Clear() {
+		_MyList.Clear();
+	}
+
 };
 
