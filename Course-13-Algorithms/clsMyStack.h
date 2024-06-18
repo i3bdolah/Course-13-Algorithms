5ec2db95cb0bab2eb2ab3b7f98e21dfb78b15f8a
i3bdolah <xbdullahx@gmail.com>
2024-06-18 18:19:33 +0300

    Adding Print in form of stack figure in the MyStack

--- Course-13-Algorithms/clsMyStack.h
+++ Course-13-Algorithms/clsMyStack.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "clsMyQueue.h"
+#include "clsDblLinkedList.h"
+#include <iomanip>
 
 using namespace std;
 
@@ -7,6 +9,19 @@ template <class EnteredType>
 class clsMyStack : public clsMyQueue <EnteredType>
 {
 public:
+	void Print() {
+		cout << "\n";
+		for (int i = 0; i < this->_MyList.Size(); i++)
+		{
+			cout << "|";
+			cout << setw(5) << this->_MyList.GetItem(i);
+			cout << "|\n";
+		}
+		cout << "-------";
+		cout << "\n";
+	}
+
+
 	void Push(EnteredType value) {
 		this->_MyList.InsertAtBeginning(value);
 	}
